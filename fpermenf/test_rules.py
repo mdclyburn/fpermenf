41dc04000fadc047a413421d2ea5605600d495a3
Marshall Clyburn <mardev2013@gmail.com>
2020-07-10 23:54:34 -0400

    Add a couple of tests for rules module

---
+++ fpermenf/test_rules.py
@@ -0,0 +1,24 @@
+import unittest
+from unittest.mock import MagicMock, patch
+
+from fpermenf import rules
+
+class RulesTestCase(unittest.TestCase):
+    def test_applicable_matches_correctly(self):
+        self.assertTrue(rules.applicable('/usr/media/video', '/usr/media/video'))
+        self.assertTrue(rules.applicable('/usr/media', '/usr/media/video'))
+        self.assertFalse(rules.applicable('/usr/media/books', '/usr/media/video'))
+        self.assertFalse(rules.applicable('/usr/media/video/sub', '/usr/media/video'))
+
+    def test_load_builds_rules(self):
+        obj = [{
+            'description': 'test rule',
+            'match': [{'file_type': 'regular'}],
+            'state': [{'owner': 'marshall'}]
+        }]
+        with patch('builtins.open', MagicMock()) as _p1:
+            with patch('json.load', MagicMock(return_value=obj)) as _p2:
+                r = rules.load('')
+                self.assertEqual(r[0].description, 'test rule')
+                self.assertEqual(r[0].match, [{'file_type': 'regular'}])
+                self.assertEqual(r[0].state, [{'owner': 'marshall'}])
